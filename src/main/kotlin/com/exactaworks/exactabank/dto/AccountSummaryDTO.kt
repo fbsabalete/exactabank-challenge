23a93ad26528a1c9cfce3e887a02ad77dde747c2
Fernando Sabalete <fb.sabalete@gmail.com>
2023-10-06 15:13:16 -0300

    feat: balance formatted to real in responses

--- src/main/kotlin/com/exactaworks/exactabank/dto/AccountSummaryDTO.kt
+++ src/main/kotlin/com/exactaworks/exactabank/dto/AccountSummaryDTO.kt
@@ -1,10 +1,12 @@
 package com.exactaworks.exactabank.dto
 
+import com.exactaworks.exactabank.extensions.toRealString
 import java.io.Serializable
 import java.math.BigDecimal
 
 
 data class AccountSummaryDTO(
-    val balance: BigDecimal? = null,
+    val balance: BigDecimal,
+    val balanceFormatted: String? = balance.toRealString(),
     val transactions: List<TransactionDTO>
 ) : Serializable
